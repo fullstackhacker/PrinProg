6d0edc56e2364a68641558f1aaedf86e54bca0b8
kapadiamush <kapadiamush@gmail.com>
2014-10-25 21:27:22 -0400

    finished optimizer and added some more tests to test various optimizers

--- Project1_tinyL/Optimizer.c
+++ Project1_tinyL/Optimizer.c
@@ -10,7 +10,6 @@
 #include <stdlib.h>
 #include "InstrUtils.h"
 #include "Utils.h"
-//#include "Instr.h"
 
 int main()
 {
@@ -22,11 +21,197 @@ int main()
 
   /* ---  FIRST: ALGEBRAIC SIMPLIFICATION PASS --- */
 
-	if (!head) {
+	if (!head) { //no instructions
 		ERROR("No instructions\n");
 		exit(EXIT_FAILURE);
 	}
 
+	if(head->next && head->next->next){  //can't optimize if there aren't enough
+		//initialization
+		instr1 = head; 
+		instr2 = head->next; 
+		instr3 = head->next->next; 
+		
+		while(instr3){
+			//handle the cases of (a + 0)(0 + a)(a - 0)(0 - a)(0 * a)(a * 0)
+			switch(instr3->opcode){ 
+			case ADD: 
+				;// blank statement
+				if(instr1->opcode == LOADI && instr2 == LOAD){ // (0 + a) 
+					if(instr1->field2 == 0){ 
+						instr2->field1 = instr3->field1; //set the register in the load to the new one	
+						instr2->next = instr3->next; //move past it
+						if(instr3->next) instr3->next->prev = instr2; //if the next one exists, point it to instr2
+						instr2->prev = instr1->prev; 
+						if(instr1->prev) instr1->prev->next = instr2;  //if the prev one exists, point it to intr2
+						if(instr1 == head) head =  instr2; 	
+						//free
+						free(instr1); 
+						free(instr3);
+						//new vars
+						instr1 = instr2; 
+						instr2 = instr1->next; 
+						instr3 = instr2->next;
+					}
+					else{ //move forward
+						instr1 = instr2; 
+						instr2 = instr3; 
+						instr3 = instr3->next;
+					}
+				} 
+				else if(instr1->opcode == LOAD && instr2->opcode == LOADI){ // (a + 0)   
+					if(instr2->field2 == 0) {
+						instr1->field1 = instr3->field1;  //set it to the register
+						instr1->next = instr3->next; 
+						if(instr3->next) instr3->next->prev = instr1; 
+						//free
+						free(instr2); 
+						free(instr3);
+						//new vars
+						instr2 = instr1->next; 
+						instr3 = instr2->next;
+					} 
+					else{ //move forward
+						instr1 = instr2; 
+						instr2 = instr3; 
+						instr3 = instr3->next;
+					}
+				}
+				else{ 
+					instr1 = instr2; 
+					instr2 = instr3; 
+					instr3 = instr3->next;
+				}
+				break;
+			case SUB: 
+				; //blank statment
+				if(instr1->opcode == LOAD && instr2->opcode == LOADI){  //(a - 0)
+					if(instr2->field2 == 0){ 
+						instr1->field1 = instr3->field1; //change the registers
+						instr1->next = instr3->next; 
+						if(instr3->next) instr3->next->prev = instr1; 
+						//free
+						free(instr2); 
+						free(instr3); 
+						//new vars
+						instr2 = instr1->next; 
+						instr3 = instr2->next; 
+					} 
+					else{ //move forward
+						instr1 = instr2; 
+						instr2 = instr3; 
+						instr3 = instr3->next;
+					}
+				} 
+				else if(instr1->opcode == LOAD && instr2->opcode == LOAD){  //(a - a)
+					if(instr1->field2 == instr2->field2){ 
+						instr3->opcode = LOADI;  //SUB -> LOADI
+						instr3->field2 = 0;  //a-a = 0
+						instr3->prev = instr1->prev;  //change prev
+						if(instr1->prev) instr1->prev->next = instr3; //change prev->next if have to 
+						if(instr1 == head) head = instr3;  //change head if have to 
+						//free
+						free(instr1); 
+						free(instr2); 
+						//new vars
+						instr1 = instr3; 
+						instr2 = instr1->next; 
+						instr3 = instr2->next;
+					} 
+					else{ //move forward
+						instr1 = instr2; 
+						instr2 = instr3; 
+						instr3 = instr3->next;
+					}
+				}
+				else{ 
+					instr1 = instr2; 
+					instr2 = instr3; 
+					instr3 = instr3->next;
+				}
+				break;
+			case MUL:
+				;//blank statement
+				if(instr1->opcode == LOADI && instr2->opcode == LOAD){ //(0 * a) || (1 * a)
+					if(instr1->field2 == 0){ //(0 * a)
+						instr1->field1 = instr3->field1; //change the load regsiter
+						instr1->next = instr3->next; //skip 2 and 3
+						if(instr3->next) instr3->next->prev = instr1; //change the prev pointer 
+						//free	
+						free(instr2); 
+						free(instr3); 
+						//repoint
+						instr2 = instr1->next; 
+						instr3 = instr2->next; 
+					}
+					else if(instr1->field2 == 1){ //(1 * a)
+						instr2->field1 = instr3->field1;  //change register
+						instr2->next = instr3->next;  //change the nex pointer
+						if(instr3->next) instr3->next->prev = instr2; 
+						instr2->prev = instr1->prev; //change prev 
+						if(instr1->prev) instr1->prev->next = instr2; 
+						if(instr1 == head) head = instr2;  //change head
+						//free
+						free(instr1); 
+						free(instr3); 
+						//repoint
+						instr1 = instr2; 
+						instr2 = instr1->next; 
+						instr3 = instr2->next;
+					} 
+					else{ //move forward
+						instr1 = instr2; 
+						instr2 = instr3; 
+						instr3 = instr3->next;
+					}
+				}
+				else if(instr1->opcode == LOAD && instr2->opcode == LOADI){ //(a * 0) || (a * 1)
+					if(instr2->field2 == 0){ 
+						instr2->field1 = instr3->field1;  //change register
+						instr2->next = instr3->next;  //change the nex pointer
+						if(instr3->next) instr3->next->prev = instr2; 
+						instr2->prev = instr1->prev; //change prev 
+						if(instr1->prev) instr1->prev->next = instr2; 
+						if(instr1 == head) head = instr2;  //change head
+						//free
+						free(instr1); 
+						free(instr3); 
+						//repoint
+						instr1 = instr2; 
+						instr2 = instr1->next; 
+						instr3 = instr2->next;
+					} 
+					else if(instr2->field2 == 1){ 
+						instr1->field1 = instr3->field1; //change the load regsiter
+						instr1->next = instr3->next; //skip 2 and 3
+						if(instr3->next) instr3->next->prev = instr1; //change the prev pointer 
+						//free	
+						free(instr2); 
+						free(instr3); 
+						//repoint
+						instr2 = instr1->next; 
+						instr3 = instr2->next; 
+					} 
+					else{
+						instr1 = instr2; 
+						instr2 = instr3; 
+						instr3 = instr3->next;
+					}
+				}
+				else{ //move forward
+					instr1 = instr2; 
+					instr2 = instr3; 
+					instr3 = instr3->next;
+				}
+				break;
+			default: 
+				instr1 = instr1->next; 
+				instr2 = instr2->next; 
+				instr3 = instr3->next; 
+				break;
+			}
+		}
+	} 
 
   /* --- SECOND: CONSTANT FOLDING PASS --- */
 
